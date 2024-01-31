c09aa8ccc9d1d97cfa566870b084bead1cbdad5e
Rae Leda <veronica.rleda@gmail.com>
2024-01-31 14:09:00 -0500

    Replaced bg

---
+++ Assets/Scripts/Ads/AdsManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+/*
+
+[RequireComponent(typeof(InterstitialAd),typeof(BannerAd))]
+public class AdsManager : MonoBehaviour, IUnityAdsLoadListener  
+{
+    public bool TestMode;
+    public string AndroidGameID;
+    public string iosGameID; 
+
+    private InterstitialAd interstitialAd;
+    public InterstitialAd Interstitial => interstitialAd; 
+    private RewardedAd rewardedAd;
+    public RewardedAd Rewarded => rewardedAd; 
+    private BannerAd bannerAd;
+    public BannerAd BannerAd => bannerAd; 
+
+    private void Awake()
+    {
+        interstitialAd = GetComponent<InterstitialAd>(); 
+        rewardedAd = GetComponent<RewardedAd>();
+        bannerAd = GetComponent<BannerAd>(); 
+
+        
+    }
+
+    public void OnInitializationComplete()
+    {
+
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+
+    }
+}
+*/ 
---
+++ Assets/Scripts/Ads/InterstitialAd.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+/*
+public class InterstitialAd : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener 
+{
+    string AndroidAdUnit = "Interstitial_Android";
+    string iOSAdUnit = "Interstitial_iOS";
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            AdUnitID = iOSAdUnit; 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/ 
